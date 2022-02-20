7f02fe0f53374aeb00beb66d6eb9199fd7e2d5ef
@exachka <95986292+AnastasiaShalukhina@users.noreply.github.com>
2022-02-20 11:53:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper(".main-slider", {
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".main-slider_arrow",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+let burger = document.querySelector(".menu-burger");
+let closeMenu = document.querySelector(".header_menu-close");
+let menuMobile = document.querySelector(".header");
+
+burger.addEventListener("click", () => {
+  menuMobile.style.display = "block";
+});
+
+closeMenu.addEventListener("click", () => {
+  menuMobile.style.display = "none";
+});
